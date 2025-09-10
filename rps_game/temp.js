81fa07d1d5e47988c190aff62831990930ebccc4
Sivesh Shrutik <114903093+phoenix643@users.noreply.github.com>
2025-09-10 17:40:03 +0530

    A basic Rock Paper Scissor Game

--- rps_game/temp.js
+++ rps_game/temp.js
@@ -1,1 +1,51 @@
-console.log("HI");
+function comp(){
+    let i = Math.floor(Math.random() * 3);
+    let j;
+    if(i == 0){
+        j = "rock";
+    }
+    else if(i == 1){
+        j = "paper";
+    }
+    else{
+        j = "scissor";
+    }
+    return j;
+}
+
+function human(){
+    let k = prompt("Rock, paper or scissor");
+    return k.toLowerCase();
+}
+
+
+
+let humanScore = 0;
+let compScore = 0;
+
+function play(co, hu){
+        if (co === hu) {
+            console.log(`Draw! Both chose ${hu}`);
+            
+    }
+
+        if (
+            (hu === "rock" && co === "scissor") ||
+            (hu === "paper" && co === "rock") ||
+            (hu === "scissor" && co === "paper")
+        ) {
+            console.log(`You win! ${hu} beats ${co}`);
+            humanScore++;
+        } else {
+            console.log(`You lose! ${co} beats ${hu}`);
+            compScore++;
+        }
+    }
+
+for(let l = 0; l<5; l += 1){
+    let hu = human();
+    let co = comp();
+    play(co,hu)
+    console.log(humanScore, compScore)    
+    
+}
